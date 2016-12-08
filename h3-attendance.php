2d7649976d0a42f78db5a52d84ea068a01f90c78
Rhyss Vivian <rhyssv@gmail.com>
2016-12-07 15:03:27 -0900

    Replaced h3-attendance.php
    Replaced h3-attendance.php from wppb.me

--- h3-attendance.php
+++ h3-attendance.php
@@ -1,79 +1,75 @@
 <?php
-/*
-Plugin Name: H3 Attendance
-Plugin URI: https://b2h3.com
-Description: A plugin designed to track and display attendance for Hash House Harrier kennels.
-Version: 0.1 Pre-Alpha
-Author: going Down On A bull moose
-Author URI: https://northof66.com
-License: GPL2
-License URI: https://www.gnu.org/licenses/gpl-2.0.html_entity_decode
-*/
 
-// Start up H3 Attendance on WordPress's "init" action hook.
-add_action( 'init', array( 'H3Attendance', 'run' ) );
-/*
-global $wpdb;
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://northof66.com
+ * @since             1.0.0
+ * @package           H3_Attendance
+ *
+ * @wordpress-plugin
+ * Plugin Name:       H3 Attendance
+ * Plugin URI:        https://github.com/rvivian/h3-attendance
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            going Down On A bull moose
+ * Author URI:        https://northof66.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       h3-attendance
+ * Domain Path:       /languages
+ */
 
-// Create the table 
-global $h3_db_version;
-$h3_db_version = '0.1';
-
-function db_install() {
-  global $wpdb;
-  $table_name = $wpdb->prefix . 'h3_user';
-  
-  $charset_collate = $wpdb->get_charset_collate();
-  
-  $sql = "CREATE TABLE $table_name (
-    id int(6) NOT NULL AUTO_INCREMENT,
-    first_name varchar(50) NULL,
-    last_name varchar(50) NULL,
-    email varchar(255) NULL,
-    hashaversary date NOT NULL,
-    home_kennel varchar(50) NULL,
-    mother_kennel varchar(50) NULL,
-    is_dog boolean NOT NULL,
-    PRIMARY KEY (id)
-  ) $charset_collate;";
-  
-  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-  dbDelta( $sql );
-  
-  add_option( 'h3_db_version', $h3_db_version );
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
 
-register_activation_hook( __FILE__, 'h3_install' );
-
-$installed_ver = get_option( "h3_db_version" );
-
-if ( $installed_ver != $h3_db_version) {
-  global $wpdb;
-  $table_name = $wpdb->prefix . 'h3_user';
-   
-  $sql = "CREATE TABLE $table_name (
-    id int(6) NOT NULL AUTO_INCREMENT,
-    first_name varchar(50) NULL,
-    last_name varchar(50) NULL,
-    email varchar(255) NULL,
-    hashaversary date NOT NULL,
-    home_kennel varchar(50) NULL,
-    mother_kennel varchar(50) NULL,
-    is_dog boolean NOT NULL,
-    PRIMARY KEY (id)
-  );";
-  
-  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-  dbDelta( $sql );
-  
-  update_option( 'h3_db_version', $h3_db_version );
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-h3-attendance-activator.php
+ */
+function activate_h3_attendance() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-h3-attendance-activator.php';
+	H3_Attendance_Activator::activate();
 }
 
-function h3_update_db_check() {
-  if ( get_site_option( 'h3_db_version' ) != $h3_db_version ) {
-    db_install();
-  }
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-h3-attendance-deactivator.php
+ */
+function deactivate_h3_attendance() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-h3-attendance-deactivator.php';
+	H3_Attendance_Deactivator::deactivate();
 }
 
-add_action( 'plugins_loaded', 'h3_update_db_check' );
-*/
+register_activation_hook( __FILE__, 'activate_h3_attendance' );
+register_deactivation_hook( __FILE__, 'deactivate_h3_attendance' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-h3-attendance.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_h3_attendance() {
+
+	$plugin = new H3_Attendance();
+	$plugin->run();
+
+}
+run_h3_attendance();
